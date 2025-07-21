c0d2c084f2511104b4e1078a7df5acd8232d358a
Alex <alexeydubentsov@gmail.com>
2025-07-22 00:25:54 +0300

    02

---
+++ L1_02.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	numbers := []int{2, 4, 6, 8, 10}
+	
+	var wg sync.WaitGroup 
+
+	for _, num := range numbers {
+		wg.Add(1)
+
+		go func(n int) {
+			defer wg.Done()
+			
+			square := n * n
+			fmt.Printf("Квадрат числа %d: %d\n", n, square)
+		}(num)
+	}
+	
+	wg.Wait()
+}
